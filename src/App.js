f5073bcaad87568c1f8a1d729d82f9c6b11a4130
junghyomoon <dsinhome70@gmail.com>
2021-04-03 15:42:43 +0900

    react needs unique key props

--- src/App.js
+++ src/App.js
@@ -2,38 +2,44 @@ import React from "react";
 // 하나의 컴포넌트만 렌더링할 수 있기 때문에, 이렇게 다른 컴포넌트를 불러와야 함.
 // 코드를 여러개의 '조각'으로 나눠, 재사용성과 유지보수를 편리하게.
 
-function Food({ name, img }) {
-	return (
-		<div>
-			<h1>I like {name}</h1>
-			<img src={img}></img>
-		</div>
-	);
-}
-
+// list내의 child는 각각 unique한 key prop을 가져야 함.
+// 모든 요소들은 "유일"해야 함.
 const foodILike = [
 	{
+		id: 1,
 		name: "kimchi",
 		image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
 	},
 	{
+		id: 2,
 		name: "gogi",
 		image:
 			"https://cdn.healthinnews.co.kr/news/photo/201910/11903_12978_1915.jpg",
 	},
 	{
+		id: 3,
 		name: "ramen",
 		image:
 			"https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg",
 	},
 ];
 
+function Food({ name, img }) {
+	return (
+		<div>
+			<h1>I like {name}</h1>
+			<img src={img} alt={name}></img>
+		</div>
+	);
+}
+
 function App() {
 	return (
 		<div>
 			<h1>hello world</h1>
 			{foodILike.map((food) => (
-				<Food name={food.name} img={food.image} />
+				// react의 내부에서만 사용되는 key prop.
+				<Food key={food.id} name={food.name} img={food.image} />
 			))}
 		</div>
 	);
