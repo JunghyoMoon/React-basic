214c9d2adf3dd6fe016ee7984ee3116f60dda53c
junghyomoon <dsinhome70@gmail.com>
2021-04-12 13:56:23 +0900

    starting movie app.

--- src/App.js
+++ src/App.js
@@ -2,48 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		console.log("hello world");
-	}
 	state = {
-		count: 0,
+		isLoading: true,
 	};
 
-	//매 순간 setState를 호출할 때마다, 새로운 state와 함께 render를 호출하게 됨.
-	add = () => {
-		this.setState((current) => ({
-			count: current.count + 1,
-		}));
-	};
-
-	sub = () => {
-		this.setState((current) => ({
-			count: current.count - 1,
-		}));
-	};
-
-	/*
-
-	*/
 	componentDidMount() {
-		console.log("component rendered");
+		setTimeout(() => {
+			this.setState({ isLoading: false });
+		}, 5000);
 	}
-	componentDidUpdate() {
-		console.log("just updated.");
-	}
-	componentWillUnmount() {
-		console.log("Adios");
-	}
+
 	render() {
-		console.log("rendering..");
-		return (
-			<div>
-				<h1>The number is : {this.state.count}</h1>
-				<button onClick={this.add}>add</button>
-				<button onClick={this.sub}>sub</button>
-			</div>
-		);
+		const { isLoading } = this.state;
+		return <div>{isLoading ? "Loading" : "We are ready"}</div>;
 	}
 }
 
